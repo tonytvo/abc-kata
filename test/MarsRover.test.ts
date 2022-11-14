0a85a76d1ce7f378fe0ce2e20b5a13c721159b75
Tony Vo <ttrung.vo@gmail.com>
2022-11-14 05:28:20 -0700

    initial staring point

---
+++ test/MarsRover.test.ts
@@ -0,0 +1,10 @@
+import { describe, test } from "@jest/globals";
+import { ABC } from "../src/MarsRover";
+import {fail} from "assert";
+
+
+describe("ABC", () => {
+  test("move", () => {
+    fail('fail test')
+  });
+});
