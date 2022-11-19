88ae148d6174a09ff95c4f0c456854df49b5759f
Tony Vo <ttrung.vo@gmail.com>
2022-11-19 12:19:37 -0700

    t add skip tests, discover some refactoring needs to be done first

--- test/ABC.test.ts
+++ test/ABC.test.ts
@@ -1,11 +1,19 @@
 import { describe, test, expect } from "@jest/globals";
 import { ABC } from "../src/ABC";
+import {fail} from "assert";
 
 
 describe("ABC rules", () => {
   test("can make word for letter A", () => {
     expect(ABCFactory.createDefaultABC().canMakeWord("A").spellResult()).toEqual(true);
   });
+
+  test.skip("once a litter on a block isused that block cannot be used again", () => {
+    //let abc = ABCFactory.createABCFromBlocks("N A", "B O").canMakeWord("A")
+    //expect(abc.containsBlock("N A")).toEqual(false);
+    fail();
+  });
+
 });
 
 class ABCFactory {
