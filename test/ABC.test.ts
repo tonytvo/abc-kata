96ccd4cbefd6adb64baa74a6d4e9de47b1920eed
Tony Vo <ttrung.vo@gmail.com>
2022-11-19 09:02:06 -0700

    r move method to factory method

--- test/ABC.test.ts
+++ test/ABC.test.ts
@@ -3,9 +3,11 @@ import { ABC } from "../src/ABC";
 
 
 describe("ABC rules", () => {
-  const createDefaultABC = () => new ABC(true);
-
   test("can make word for letter A", () => {
-    expect(createDefaultABC().canMakeWord("A").spellResult()).toEqual(true);
+    expect(ABCFactory.createDefaultABC().canMakeWord("A").spellResult()).toEqual(true);
   });
 });
+
+class ABCFactory {
+  static createDefaultABC = () => new ABC(true);
+}
